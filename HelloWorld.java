9598afa5edfc9334e6314fd10984de30f7fa6f65
gadepallip <praveen.gadepalli@gmail.com>
2019-12-24 14:14:42 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
 public static void main(String[] args){
    //some comment
-System.out.println("Hello World - Test for Jenkins");
+System.out.println("Hello World - Test for Jenkins, testing for build on commit");
    }
 }
